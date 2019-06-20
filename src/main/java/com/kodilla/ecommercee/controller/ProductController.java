ff0fb410afc2a06953786901540e8902678765e6
AnnaSzczech <anna.szczech89@gmail.com>
2019-06-20 20:38:04 +0200

    RequestParam && RequestBody added

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,9 +1,7 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.domain.ProductDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,19 +16,19 @@ public class ProductController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getProduct")
-    public ProductDto getProduct(Long productId){
+    public ProductDto getProduct(@RequestParam("productId") Long productId){
         return new ProductDto(1L, "product1", "description1", 1.00);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createProduct")
-    public void createProduct(ProductDto product){}
+    public void createProduct(@RequestBody ProductDto product){}
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
-    public ProductDto updateProduct(ProductDto product){
-        return new ProductDto(1L, "Edited product1", "Edited description1", 1.00);
+    public ProductDto updateProduct(@RequestBody ProductDto product){
+        return product;
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
-    public void Product(Long productId){}
+    public void Product(@RequestParam("productId") Long productId){}
 
 }
