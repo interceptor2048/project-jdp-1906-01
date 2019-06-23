5c17569e8f050c61e91e87a4e9337553de84658e
AnnaSzczech <anna.szczech89@gmail.com>
2019-06-23 13:13:18 +0200

    Encja Order - implementacja

--- src/main/java/com/kodilla/ecommercee/domain/OrderEntity.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderEntity.java
@@ -1,4 +1,63 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "ORDERS")
 public class OrderEntity {
+    private Long id;
+    private int quantity;
+    private ProductEntity product;
+    private UserEntity user;
+
+
+    public OrderEntity(Long id, int quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORDER_ID", unique = true)
+    public Long getId(){
+        return id;
+    }
+
+    @Column(name = "QUANTITY")
+    @NotNull
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "PRODUCT_ID")
+    public ProductEntity getProduct() {
+        return product;
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "USER")
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setProduct(ProductEntity product){
+        this.product = product;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
