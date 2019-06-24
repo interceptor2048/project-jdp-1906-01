c083495b3817f66dc028e71e773662b27ef8cc5b
AnnaSzczech <anna.szczech89@gmail.com>
2019-06-24 19:10:10 +0200

    Entity Order - implementation

--- src/test/java/com/kodilla/ecommercee/EcommerceeApplicationTests.java
+++ src/test/java/com/kodilla/ecommercee/EcommerceeApplicationTests.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EcommerceeApplicationTests {
 
-    @Ignore
     @Test
     public void contextLoads() {
     }
