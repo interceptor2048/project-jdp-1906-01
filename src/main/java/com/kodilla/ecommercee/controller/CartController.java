68ae046b057339dd400f07de744efc73998853ac
adakacz <a.j.kaczorowski@gmail.com>
2019-06-24 23:49:33 +0200

    Task 3 - dodanie listy endpointów carts

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,6 +1,34 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.dto.CartDto;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/ecommercee/cart")
 public class CartController {
+    @RequestMapping(method = RequestMethod.POST, value = "createCart")
+    public void createCart(@RequestBody CartDto cart){}
 
+    @RequestMapping(method = RequestMethod.GET, value = "getProductsFromCart")
+    public CartDto getProducts(@RequestParam("productsId") Long productsId){
+        return new CartDto(1, 2);
+    }
 
+    @RequestMapping(method = RequestMethod.GET, value = "addProductsToCart")
+    public CartDto addProducts(@RequestParam("productsId") Long productsId) {
+        return new CartDto(1, 2);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProductFromCart")
+    public CartDto deleteProduct(@RequestParam("productId") Long productsId) {
+        return new CartDto(1, 2);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    public CartController createOrder(@RequestBody CartDto order){}
 }
---
+++ src/main/java/com/kodilla/ecommercee/domain/dto/CartDto.java
@@ -0,0 +1,16 @@
+package com.kodilla.ecommercee.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CartDto {
+    private Long id;
+    private Long productId;
+}
