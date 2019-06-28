dc261cdc5c7772cdd3afacf471195b077f8eb361
majic1985 <majic1985.mk@gmail.com>
2019-06-28 14:32:26 +0200

    JDP190601-14

--- src/main/java/com/kodilla/ecommerce/domain/CartEntity.java
+++ src/main/java/com/kodilla/ecommerce/domain/CartEntity.java
@@ -4,27 +4,31 @@ package com.kodilla.ecommerce.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "CART")
 public class CartEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID", unique = true)
-    private Long cartId;
     //    RELACJE MIĘDZY KLASAMI SĄ ZAKOMENTOWANE ZE WZGLĘDU NA BRAK ENCJI USER i GROUP
-    //@ManyToMany(cascade = CascadeType.ALL, mappedBy = "cart")
-   // private List<ProductEntity> productList = new ArrayList<>();
+    //@ManyToMany(cascade = CascadeType.ALL)
+   // @JoinTable(
+            //name = "JOIN_CART_PRODUCT",
+           // joinColumns = {@JoinColumn(name = "CART_ID", referencedColumnName = "CART_ID")},
+           // inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")}
+   // )
+  //  @MapKeyColumn(name = "PRODUCT_QUANTITY")
+  //  private Map<Integer,ProductEntity> quantityOfProductsInCart = new HashMap<>();
 
-   // @OneToOne(cascade = CascadeType.ALL, mappedBy = "cart")
-    // private UserEntity user;
-
-    //@MapKeyColumn(name = "PRODUCT_QUANTITY")
-    // private Map<Integer, CartEntity> quantityOfProductsInCart = new HashMap<>();
+   // @OneToOne(cascade =  CascadeType.ALL, fetch = FetchType.EAGER)
+   // private UserEntity user;
 }
 
