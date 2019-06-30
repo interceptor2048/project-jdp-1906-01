062bc86276adcb9dc0c534271c400fad681a6cf9
adakacz <a.j.kaczorowski@gmail.com>
2019-06-30 11:00:02 +0200

    deleted group controller from task 3  as it is separated task 2

--- src/main/java/com/kodilla/ecommerce/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommerce/controller/GroupController.java
@@ -1,31 +1,6 @@
 package com.kodilla.ecommerce.controller;
 
-import com.kodilla.ecommerce.domain.dto.GroupDto;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-@RestController
-@RequestMapping("/v1/ecommercee/group")
 public class GroupController {
-    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
-    public List<GroupDto> getGroups() {
-        return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createGroup")
-    public void createGroup(@RequestBody GroupDto group) {
-    }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getGroup")
-    public GroupDto getGroup(@RequestParam("groupId") Long groupId) {
-        return new GroupDto(1L);
-    }
-
-    @RequestMapping(method = RequestMethod.PUT, value = "updateGroup")
-    public GroupDto updateGroup(@RequestBody GroupDto group) {
-        return group;
-    }
-}
