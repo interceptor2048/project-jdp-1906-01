7914851b4d24d26a8a875f7ffa4e78ac1ff6a9a7
AnnaSzczech <anna.szczech89@gmail.com>
2019-07-07 23:09:13 +0200

    Bug-JDP190601-24

--- src/test/java/com/kodilla/ecommerce/domain/OrderEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommerce/domain/OrderEntityTestSuite.java
@@ -18,44 +18,44 @@ import java.sql.SQLException;
 
 import static org.junit.Assert.*;
 
-//@RunWith(SpringRunner.class)
-////@SpringBootTest
-//@DataJpaTest
-//@AutoConfigureTestDatabase
-//public class OrderEntityTestSuite {
-//
-//    @Autowired
-//    private TestEntityManager testEntityManager;
-//
-//    @Autowired
-//    private OrderEntityRepository orderEntityRepository;
-//
-//    @Autowired
-//    private OrderProductEntityRepository orderProductEntityRepository;
-//
-//    @Test
-//    public void testAddingSingleOrderToOrderEntity(){
-//        //given
-//        OrderEntity order1 = new OrderEntity();
-//        OrderEntity order2 = new OrderEntity();
-//        UserEntity user = new UserEntity("name", false);
-//        user.getOrders().add(order1);
-//        user.getOrders().add(order2);
-//        order1.setUser(user);
-//        order2.setUser(user);
-//
-//        //when
-//        OrderEntity saveInDB = testEntityManager.persist(order1);
-//        OrderEntity getFromDB = orderEntityRepository.save(saveInDB);
-//        Long id = order1.getId();
-//        Long idUser = user.getId();
-//
-//        //then
-//        assertEquals(getFromDB, saveInDB);
-//        assertEquals(id, saveInDB.getId());
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class OrderEntityTestSuite {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private OrderEntityRepository orderEntityRepository;
+
+    @Autowired
+    private OrderProductEntityRepository orderProductEntityRepository;
+
+    @Test
+    public void testAddingSingleOrderToOrderEntity(){
+        //given
+        OrderEntity order1 = new OrderEntity();
+        OrderEntity order2 = new OrderEntity();
+        UserEntity user = new UserEntity("name", false);
+        user.getOrders().add(order1);
+        user.getOrders().add(order2);
+        order1.setUser(user);
+        order2.setUser(user);
+
+        //when
+        OrderEntity saveInDB = testEntityManager.persist(order1);
+        OrderEntity getFromDB = orderEntityRepository.save(saveInDB);
+        Long id = order1.getId();
+        Long idUser = user.getId();
+
+        //then
+        assertEquals(getFromDB, saveInDB);
+        assertEquals(id, saveInDB.getId());
 //        assertEquals(idUser, saveInDB.getUser().getId());
-//    }
-//
+    }
+
 //    @Test
 //    public void testAddingProductsToOrderEntity(){
 //        //given
@@ -84,64 +84,7 @@ import static org.junit.Assert.*;
 //        //then
 //        assertEquals(getFromDB, saveInDB);
 //        assertEquals(id, saveInDB.getId());
-//        assertTrue(saveInDB.getProduct().equals(product1));
+////        assertTrue(saveInDB.getProduct().equals(product1));
 //    }
-//}
-
-
-//TEST KTORY DODAJE DANE DO BAZY MYSQL
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class OrderEntityTestSuite {
-//
-////    @Autowired
-////    private TestEntityManager testEntityManager;
-//
-//    @Autowired
-//    private OrderEntityRepository orderEntityRepository;
-//
-//    @Autowired
-//    private OrderProductEntityRepository orderProductEntityRepository;
-//
-//    @Test
-//    public void testAddingProductsToOrderEntity(){
-//        //given
-//        OrderEntity order = new OrderEntity();
-//        UserEntity user = new UserEntity("Anna Szczech", true);
-//        user.getOrders().add(order);
-//        order.setUser(user);
-//
-//        ProductEntity product1 = new ProductEntity("product1", "description1", 5.50);
-//        ProductEntity product2 = new ProductEntity("product2", "description2", 2.40);
-//
-//        OrderProduct orderProduct1 = new OrderProduct(order, product1, 3);
-//        OrderProduct orderProduct2 = new OrderProduct(order, product2, 2);
-//
-//        product1.getOrders().add(orderProduct1);
-//        product2.getOrders().add(orderProduct2);
-//
-//        order.getProducts().add(orderProduct1);
-//        order.getProducts().add(orderProduct2);
-//
-//        orderProductEntityRepository.save(orderProduct1);
-//    }
-//}
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class OrderEntityTestSuite {
-
-    @Autowired
-    private ProductEntityRepository productEntityRepository;
-
-    @Test
-    public void testAddingProductsToOrderEntity() {
-        //given
-        ProductEntity product1 = new ProductEntity("product1", "description1", 5.50);
-//        ProductEntity product2 = new ProductEntity("product2", "description2", 2.40);
-
-
-        productEntityRepository.save(product1);
-//        productEntityRepository.save(product2);
-    }
 }
+
--- src/test/java/com/kodilla/ecommerce/domain/UserEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommerce/domain/UserEntityTestSuite.java
@@ -12,90 +12,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
-//
-//@RunWith(SpringRunner.class)
-//@DataJpaTest
-//public class UserEntityTestSuite {
-//
-//    @Autowired
-//    private TestEntityManager testEntityManager;
-//
-//    @Autowired
-//    private UserEntityRepository userEntityRepository;
-//
-//    @Autowired
-//    private OrderProductEntityRepository orderProductEntityRepository;
-//
-//    @Test
-//    public void testAddingSingleUserToUserEntity(){
-//        //given
-//        UserEntity user = new UserEntity("name", false);
-//
-//        //when
-//        UserEntity saveInDB = testEntityManager.persist(user);
-//        UserEntity getFromDB = userEntityRepository.save(saveInDB);
-//        Long id = user.getId();
-//
-//        //then
-//        assertEquals(getFromDB, saveInDB);
-//        assertEquals(id, saveInDB.getId());
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void testAddingOrderToEntity(){
-//        //given
-//        OrderEntity order1 = new OrderEntity();
-//        OrderEntity order2 = new OrderEntity();
-//        UserEntity user = new UserEntity("name", false);
-//        user.getOrders().add(order1);
-//        user.getOrders().add(order2);
-//        order1.setUser(user);
-//        order2.setUser(user);
-//
-//        ProductEntity product1 = new ProductEntity("product1", "description1", 2.50);
-//        ProductEntity product2 = new ProductEntity("product2", "description2", 5.50);
-//        ProductEntity product3 = new ProductEntity("product3", "description3", 10.50);
-//
-//        OrderProduct orderProduct1 = new OrderProduct(order1, product1, 3);
-//        OrderProduct orderProduct2 = new OrderProduct(order1, product2, 1);
-//        OrderProduct orderProduct3 = new OrderProduct(order2, product2, 4);
-//        OrderProduct orderProduct4 = new OrderProduct(order2, product3, 2);
-//
-//        product1.getOrders().add(orderProduct1);
-//        product2.getOrders().add(orderProduct2);
-//        product2.getOrders().add(orderProduct3);
-//        product3.getOrders().add(orderProduct4);
-//        order1.getProducts().add(orderProduct1);
-//        order1.getProducts().add(orderProduct2);
-//        order2.getProducts().add(orderProduct3);
-//        order2.getProducts().add(orderProduct4);
-//
-//        //when
-//        OrderProduct saveInDB = testEntityManager.persist(orderProduct1);
-//        OrderProduct getFromDB = orderProductEntityRepository.save(saveInDB);
-//        Long id = orderProduct1.getId();
-//
-//        //then
-//        assertEquals(getFromDB, saveInDB);
-//        assertEquals(id, saveInDB.getId());
-//        assertTrue(saveInDB.getProduct().equals(product1));
-//        assertTrue(saveInDB.getOrder().equals(order1));
-//        assertEquals(3, saveInDB.getQuantity());
-//    }
-//
-//    //Gdy powstanie Encja Cart trzeba dodać testAddingCartToEntity !!!
-//
-//}
-//
-//
-//
-//
-//TEST KTORY DODAJE DANE DO BAZY MYSQL
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class UserEntityTestSuite {
 
+    @Autowired
+    private TestEntityManager testEntityManager;
 
     @Autowired
     private UserEntityRepository userEntityRepository;
@@ -106,9 +29,64 @@ public class UserEntityTestSuite {
     @Test
     public void testAddingSingleUserToUserEntity(){
         //given
-        UserEntity user = new UserEntity("Anna Szczech", false);
+        UserEntity user = new UserEntity("name", false);
 
         //when
-        userEntityRepository.save(user);
+        UserEntity saveInDB = testEntityManager.persist(user);
+        UserEntity getFromDB = userEntityRepository.save(saveInDB);
+        Long id = user.getId();
+
+        //then
+        assertEquals(getFromDB, saveInDB);
+        assertEquals(id, saveInDB.getId());
     }
+
+    @Test
+    public void testAddingOrderToEntity(){
+        //given
+        OrderEntity order1 = new OrderEntity();
+        OrderEntity order2 = new OrderEntity();
+        UserEntity user = new UserEntity("name", false);
+        user.getOrders().add(order1);
+        user.getOrders().add(order2);
+        order1.setUser(user);
+        order2.setUser(user);
+
+        ProductEntity product1 = new ProductEntity("product1", "description1", 2.50);
+        ProductEntity product2 = new ProductEntity("product2", "description2", 5.50);
+        ProductEntity product3 = new ProductEntity("product3", "description3", 10.50);
+
+        OrderProduct orderProduct1 = new OrderProduct(order1, product1, 3);
+        OrderProduct orderProduct2 = new OrderProduct(order1, product2, 1);
+        OrderProduct orderProduct3 = new OrderProduct(order2, product2, 4);
+        OrderProduct orderProduct4 = new OrderProduct(order2, product3, 2);
+
+        product1.getOrders().add(orderProduct1);
+        product2.getOrders().add(orderProduct2);
+        product2.getOrders().add(orderProduct3);
+        product3.getOrders().add(orderProduct4);
+        order1.getProducts().add(orderProduct1);
+        order1.getProducts().add(orderProduct2);
+        order2.getProducts().add(orderProduct3);
+        order2.getProducts().add(orderProduct4);
+
+        //when
+        OrderProduct saveInDB = testEntityManager.persist(orderProduct1);
+        OrderProduct getFromDB = orderProductEntityRepository.save(saveInDB);
+        Long id = orderProduct1.getId();
+
+        //then
+        assertEquals(getFromDB, saveInDB);
+        assertEquals(id, saveInDB.getId());
+        assertTrue(saveInDB.getProduct().equals(product1));
+        assertTrue(saveInDB.getOrder().equals(order1));
+        assertEquals(3, saveInDB.getQuantity());
+    }
+
+    //Gdy powstanie Encja Cart trzeba dodać testAddingCartToEntity !!!
+
 }
+
+
+
+
